1e637497b3e27dd2658c56fb292b407bd4765f09
michaelsatumba <mjsatumba@gmail.com>
2022-01-19 12:09:19 -0800

    header for main pages

--- screens/GithubScreen.js
+++ screens/GithubScreen.js
@@ -1,16 +1,37 @@
-import { View, Text, Button } from 'react-native';
+import {
+	View,
+	Text,
+	Button,
+	SafeAreaView,
+	TouchableOpacity,
+	Image,
+} from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import tw from 'tailwind-react-native-classnames';
+import { Ionicons } from '@expo/vector-icons';
 
 const GithubScreen = () => {
 	const navigation = useNavigation();
 	return (
-		<View>
-			<Text>Github</Text>
-			<Text>Github</Text>
-			<Text>Github</Text>
-			<Button title="go back" onPress={() => navigation.navigate('Home')} />
-		</View>
+		<SafeAreaView>
+			{/* Header */}
+			<View style={tw`flex-row items-center justify-between px-5 mb-5`}>
+				<TouchableOpacity onPress={() => navigation.goBack()} style={tw`p-2`}>
+					<Ionicons name="chevron-back-outline" size={34} color="#F8ad05" />
+				</TouchableOpacity>
+
+				<Text style={tw`text-xl font-bold pl-2`}>GitHub</Text>
+
+				<TouchableOpacity onPress={() => navigation.navigate('Testimonial')}>
+					<Image
+						style={tw`h-10 w-10 rounded-full`}
+						source={require('../assets/papareactlogo.png')}
+					/>
+				</TouchableOpacity>
+			</View>
+			{/* End of Header */}
+		</SafeAreaView>
 	);
 };
 
