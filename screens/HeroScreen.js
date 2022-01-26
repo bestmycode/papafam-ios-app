2185990fb0b5691eea76b1b7b22d7e142f8ed0bb
michaelsatumba <mjsatumba@gmail.com>
2022-01-25 16:14:53 -0800

    courseList

--- screens/HeroScreen.js
+++ screens/HeroScreen.js
@@ -5,18 +5,23 @@ import {
 	SafeAreaView,
 	TouchableOpacity,
 	Image,
+	ScrollView,
 } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import tw from 'tailwind-react-native-classnames';
 import { Ionicons } from '@expo/vector-icons';
 import Header from '../components/Header';
+import useAuth from '../hooks/useAuth';
+import CourseList from '../components/CourseList';
 
 const HeroScreen = () => {
 	const navigation = useNavigation();
+	const { user } = useAuth();
 	return (
 		<SafeAreaView>
 			<Header title="Zero to Full Stack Hero" />
+			<CourseList />
 		</SafeAreaView>
 	);
 };
